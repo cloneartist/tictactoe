86eff29f65fdc6a58638648fbcfeed1ab6656b22
cloneartist <73981868+cloneartist@users.noreply.github.com>
2021-04-28 22:59:24 +0530

    Basic Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+void main()=> runApp(new MyApp());
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     theme:new ThemeData(primaryColor: Colors.black),
+     home: new HomePage(),
+
+    );
+  }
+}
